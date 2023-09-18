ca25c12d7d8989a3eb424b9f65260c689f63237d
starlighter4097 <starlighter4097@gmail.com>
2023-09-19 00:29:51 +0530

    fixed write failed to disk of install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/starlighter4097/smartDots.git -b main ~ 
+cd ~/smartDots 
+git remote set-url origin git@github.com:starlighter4097/smartDots.git 
+bash ./setup.sh 
+echo "setting up of stow successful"
+bash ~/basescripts/basescripts.sh
+
+# command is :curl -Lo dotsetup https://t.ly/kUbCB | bash dotsetup
