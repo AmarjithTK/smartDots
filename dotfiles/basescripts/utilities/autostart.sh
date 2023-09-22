9a0d27ff43313596ea6e290c000d87ffaa59135e
starlighter4097 <starlighter4097@gmail.com>
2023-09-22 22:37:36 +0530

    autostart qbittorrent

--- dotfiles/basescripts/utilities/autostart.sh
+++ dotfiles/basescripts/utilities/autostart.sh
@@ -12,7 +12,7 @@ redshifter 3000 .9 &
 # nitrogen --restore &
 flameshot &
 nm-applet &
-volctl &
 slstatus &
 kdeconnect-cli -l& 
 feh --recursive --bg-fill --randomize ~/.wallpapers &
+qbittorrent &
