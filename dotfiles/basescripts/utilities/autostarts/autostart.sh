73a7f6ed8056cd1c64b1a2b5781e51ff8da8cc7f
AmarjithTK <amarjithraveendran@gmail.com>
2023-10-06 19:38:55 +0530

    created new bin structure with bin scripts on basescripts folder and binary compiled files on bin folder, updated polybar to show either wlan or eth but not both

---
+++ dotfiles/basescripts/utilities/autostarts/autostart.sh
@@ -0,0 +1,18 @@
+
+
+
+# Todo - add window manager independant autostart scripts
+# add - wallpaper downloader script that 
+#
+
+blueman-applet &
+shutdown "2:00" &
+sxhkd &
+redshifter 3000 .9 &
+# nitrogen --restore &
+flameshot &
+nm-applet &
+slstatus &
+kdeconnect-cli -l& 
+feh --recursive --bg-fill --randomize ~/.wallpapers &
+qbittorrent &
