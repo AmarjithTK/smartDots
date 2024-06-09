1323f1b47efba36d2a04d1fcad944ce338069f16
AmarjithTK <amarjithraveendran@gmail.com>
2024-06-09 17:12:38 +0530

    added battery charging status to notifybattery script

--- dotfiles/basescripts/utilities/notifybattery.sh
+++ dotfiles/basescripts/utilities/notifybattery.sh
@@ -7,7 +7,7 @@ SOUND_FILE="~/.helpers/musics/low_battery.mp3"
 BATTERY_THRESHOLD=45
 
 # Notification duration in seconds (2 minutes)
-NOTIFICATION_DURATION=30
+NOTIFICATION_DURATION=15
 
 # Function to play the notification sound in a loop for the specified duration
 play_notification_sound() {
@@ -21,13 +21,16 @@ while true; do
     # Get the current battery level
     battery_level=$(upower -i $(upower -e | grep BAT) | grep --color=never -E percentage|xargs|cut -d' ' -f2|sed s/%//)
 
-    # Check if the battery level is below the threshold
-    if [ "$battery_level" -lt "$BATTERY_THRESHOLD" ]; then
+    # Get the charging status
+    charging_status=$(upower -i $(upower -e | grep BAT) | grep --color=never -E state|xargs|cut -d' ' -f2)
+
+    # Check if the battery level is below the threshold and the battery is not charging
+    if [ "$battery_level" -lt "$BATTERY_THRESHOLD" ] && [ "$charging_status" != "charging" ]; then
         # Play the notification sound
         play_notification_sound
     fi
 
     # Wait for a minute before checking again
-    sleep 600
+    sleep 300
 done
 
