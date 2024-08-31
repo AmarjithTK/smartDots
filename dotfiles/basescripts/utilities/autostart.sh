75e75308be85f2838fede9ef4a30794cedb98a9a
AmarjithTK <amarjithraveendran@gmail.com>
2024-09-01 02:49:30 +0530

    fixed async problem with sfwallpaper and reduced delay to 10seconds

--- dotfiles/basescripts/utilities/autostart.sh
+++ dotfiles/basescripts/utilities/autostart.sh
@@ -20,26 +20,29 @@ xfce4-power-manager &
 
 pkill -x sxhkd ; sxhkd -c ~/.config/sxhkd/bspwm-sxhdrc ~/.config/sxhkd/alone-sxhkdrc &
 pkill -x polybar ; polybar &
-xrandr --output eDP-1 --mode 1368x768 
 
 
 bash ~/basescripts/utilities/livewall.sh &
+
+#bash ~/basescripts/utilities/sfwallpaper.sh > $HOME/output-log 2>&1 &
 bash ~/basescripts/utilities/greeterwall.sh &
 bash ~/basescripts/utilities/notifyidle.sh &
 bash ~/basescripts/utilities/bedtime.sh &
 
-#
 
 # Load device-specific settings based on hostname
 HOSTNAME=$(hostname)
 
 if [[ "$HOSTNAME" == *"home-pc"* ]]; then
-  bash ~/basescripts/utilities/volume_regulator.sh
+  bash ~/basescripts/utilities/volume_regulator.sh &
 elif [[ "$HOSTNAME" == *"work-laptop"* ]]; then
   bash ~/basescripts/utilities/notifybattery.sh &
+  xrandr --output eDP-1 --mode 1920x1080 & 
   xautolock -time 10 -locker "i3lock" &
 else
   echo "No specific configuration for this hostname."
 fi
 
+sleep 10
+bash ~/basescripts/utilities/sfwallpaper.sh &
 
