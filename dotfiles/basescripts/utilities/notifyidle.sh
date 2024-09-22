7658d40ed0ea42738ea488bd85929300298f6d22
AmarjithTK <amarjithraveendran@gmail.com>
2024-09-22 19:55:09 +0530

    fixed notifyidle not detecting chrome playback

--- dotfiles/basescripts/utilities/notifyidle.sh
+++ dotfiles/basescripts/utilities/notifyidle.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Path to the sound file you want to play
-SOUND_FILE="$HOME/.helpers/musics/idle.mp3"
+SOUND_FILE="~/.helpers/musics/idle.mp3"
 
 # Idle time threshold in milliseconds (10 minutes = 600000 milliseconds)
-IDLE_TIME_THRESHOLD=10000
+IDLE_TIME_THRESHOLD=1800000
 
 # Notification duration in seconds (2 minutes)
 NOTIFICATION_DURATION=30
@@ -13,39 +13,31 @@ NOTIFICATION_DURATION=30
 play_notification_sound() {
     end_time=$((SECONDS + NOTIFICATION_DURATION))
     while [ $SECONDS -lt $end_time ]; do
-        paplay "$SOUND_FILE"
+        eval "paplay $SOUND_FILE"
     done
 }
-
-# Function to check if any audio playback is running
 is_audio_playing() {
-    # Get the list of running audio streams using pactl
-    audio_streams=$(pactl list short sink-inputs)
-
-    # Check if there are any active audio streams
-    if [ -n "$audio_streams" ]; then
-        return 1  # true, audio is playing
+    if pactl list sink-inputs | grep -q "Corked: no" && pactl list sink-inputs | grep -q "Mute: no"; then
+        return 0  # True
     else
-        return 0  # false, no audio playing
+        return 1 # False
     fi
 }
-
-# Main loop
 while true; do
     # Get the current idle time in milliseconds
     idle_time=$(xprintidle)
-    echo "Idle time: $idle_time ms"
+    echo $idle_time
 
     # Check if the idle time exceeds the threshold
     if [ "$idle_time" -gt "$IDLE_TIME_THRESHOLD" ]; then
-        # Check if audio is being played
+        # Check if audio or video is being played
         if ! is_audio_playing; then
             # Play the notification sound
             play_notification_sound
         fi
     fi
 
-    # Wait for 30 seconds before checking again
-    sleep 1
+    # Wait for a minute before checking again
+    sleep 30
 done
 
