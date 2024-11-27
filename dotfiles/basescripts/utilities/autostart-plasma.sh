80ab3e605f3b47450772fbd240fca591cf2574b5
AmarjithTK <amarjithraveendran@gmail.com>
2024-11-28 01:30:52 +0530

    added simple brightness control and removed unwanted stuff

--- dotfiles/basescripts/utilities/autostart-plasma.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-bash ~/basescripts/utilities/foodalarm.sh
-bash ~/basescripts/utilities/bedtime.sh
-bash ~/basescripts/utilities/notifybattery.sh
--- dotfiles/basescripts/utilities/autostart.sh
+++ dotfiles/basescripts/utilities/autostart.sh
@@ -1,54 +1,57 @@
+#!/bin/bash
 
-bash ~/basescripts/utilities/sfwallpaper.sh &
-bash ~/basescripts/utilities/foodalarm.sh &
+# Function for BSPWM environment
+autostart_bspwm() {
+  # Start utilities
+  bash ~/basescripts/utilities/sfwallpaper.sh &
+  bash ~/basescripts/utilities/foodalarm.sh &
 
+  blueman-applet &
+  nm-applet &
+  kdeconnect-cli -l & 
+  numlockx &
 
-# Todo - add window manager independant autostart scripts
-# add - wallpaper downloader script that 
-#
+  xfce4-power-manager &
 
-blueman-applet &
-#sxhkd &
-#redshifter 3000 .9 &
-# nitrogen --restore &
-#flameshot &
-nm-applet &
-#slstatus &
-kdeconnect-cli -l& 
-#feh --recursive --bg-fill --randomize ~/.wallpapers &
-numlockx &
+  # Reload sxhkd and polybar
+  pkill -x sxhkd ; sxhkd -c ~/.config/sxhkd/bspwm-sxhdrc ~/.config/sxhkd/alone-sxhkdrc &
+  pkill -x polybar ; polybar &
 
-xfce4-power-manager &
+  # Start compositor
+  picom &
 
-pkill -x sxhkd ; sxhkd -c ~/.config/sxhkd/bspwm-sxhdrc ~/.config/sxhkd/alone-sxhkdrc &
-pkill -x polybar ; polybar &
+  # Run additional scripts
+  bash ~/basescripts/utilities/greeterwall.sh &
+  bash ~/basescripts/utilities/bedtime.sh &
 
-picom &
+  # Load device-specific settings based on hostname
+  HOSTNAME=$(hostname)
+  if [[ "$HOSTNAME" == *"home-pc"* ]]; then
+    xrandr --output eDP-1 --auto --output HDMI-1 --same-as eDP-1
+  elif [[ "$HOSTNAME" == *"work-laptop"* ]]; then
+    xrandr --output eDP-1 --auto
+    bash ~/basescripts/utilities/notifybattery.sh &
+    bash ~/basescripts/utilities/grayscale_toggle.sh
+  else
+    echo "No specific configuration for this hostname."
+  fi
+}
 
-#bash ~/basescripts/utilities/sfwallpaper.sh > $HOME/output-log 2>&1 &
-bash ~/basescripts/utilities/greeterwall.sh &
-bash ~/basescripts/utilities/bedtime.sh &
-bash ~/basescripts/utilities/brightness_control.sh apply
+# Blank function for Plasma environment
+autostart_plasma() {
+bash ~/basescripts/utilities/foodalarm.sh
+bash ~/basescripts/utilities/bedtime.sh
+bash ~/basescripts/utilities/notifybattery.sh
 
+  pkill -x sxhkd ; sxhkd -c ~/.config/sxhkd/alone-sxhkdrc &
+}
 
-
-# Load device-specific settings based on hostname
-HOSTNAME=$(hostname)
- 
-if [[ "$HOSTNAME" == *"home-pc"* ]]; then
-  bash ~/basescripts/utilities/volume_regulator.sh &
-  xrandr --output eDP-1 --auto --output HDMI-1 --same-as eDP-1
-elif [[ "$HOSTNAME" == *"work-laptop"* ]]; then
-
-  xrandr --output eDP-1 --off --output HDMI-1 --auto
-  bash ~/basescripts/utilities/notifyidle.sh &
-  bash ~/basescripts/utilities/notifybattery.sh &
-#  xrandr --output eDP-1 --mode 1368x768 & 
-  sleep 10
-  bash ~/basescripts/utilities/grayscale_toggle.sh
-  xautolock -time 30 -locker "i3lock -i $HOME/Pictures/cat_wallpaper.jpg" &
+# Main logic: parse the environment argument
+if [[ "$1" == "--environment=bspwm" ]]; then
+  autostart_bspwm
+elif [[ "$1" == "--environment=plasma" ]]; then
+  autostart_plasma
 else
-  echo "No specific configuration for this hostname."
+  echo "Usage: $0 --environment=bspwm|plasma"
+  exit 1
 fi
-
-
