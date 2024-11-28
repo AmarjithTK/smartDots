731088aed07e06345b5e21b7918f346a88ec140f
AmarjithTK <amarjithraveendran@gmail.com>
2024-11-28 10:56:23 +0530

    fedora version of dotfiles

--- setup.sh
+++ setup.sh
@@ -3,13 +3,8 @@
 
 
 # Check if stow is installed
-if ! pacman -Q stow &> /dev/null; then
-    echo "Stow is not installed. Installing..."
-    sudo pacman -S stow --noconfirm
-    echo "Stow has been installed."
-else
-    echo "Stow is already installed."
-fi
+if ! command -v stow &> /dev/null; then sudo dnf install -y stow; fi
+
 
 
 # stow dotfiles -t ~/ *
